3c038ce9136dfa5426fc42638a6793b139c9a493
TheGameFather <f15gdsy@gmail.com>
2015-01-08 15:41:26 +0800

    - Make default root folder different for different platforms.

--- Scripts/Utilities.cs
+++ Scripts/Utilities.cs
@@ -7,7 +7,31 @@ namespace FileExplorer {
 
 	public static class Utilities {
 		public static string GetUserRoot () {
-			return System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+			System.Environment.SpecialFolder rootFolder;
+
+			switch (Application.platform) {
+			case RuntimePlatform.OSXPlayer:
+			case RuntimePlatform.OSXEditor:
+			case RuntimePlatform.OSXWebPlayer:
+				rootFolder = System.Environment.SpecialFolder.Personal;
+				break;
+
+			case RuntimePlatform.WindowsPlayer:
+			case RuntimePlatform.WindowsEditor:
+			case RuntimePlatform.WindowsWebPlayer:
+				rootFolder = System.Environment.SpecialFolder.MyComputer;
+				break;
+
+			case RuntimePlatform.LinuxPlayer:
+				rootFolder = System.Environment.SpecialFolder.Personal;
+				break;
+
+			default:
+				rootFolder = System.Environment.SpecialFolder.Personal;
+				break;
+			}
+
+			return System.Environment.GetFolderPath(rootFolder);
 		}
 
 		public static List<FileSystemInfo> GetFilesInDirectory (string directoryPath, bool includeHidden = false) {
